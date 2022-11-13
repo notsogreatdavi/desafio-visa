c61cfbbbbb29374a873e5193879c12d0b0b49bbf
Rosendo <ewerton.rosendo15@gmail.com>
2022-11-13 15:19:51 -0300

    site

--- visa_project/web_site/views.py
+++ visa_project/web_site/views.py
@@ -8,8 +8,10 @@ from .models import Search
 def home(request):
     return render(request, "home.html")
 
+
 def acessibilidade(request):
     return render(request, "acessibilidade.html")
+
 
 def map(request):
 
@@ -22,6 +24,7 @@ def map(request):
         form = SearchForm()
 
     # pegando o local do form
+
     address = Search.objects.all().last()
     location = geocoder.osm(address)
     lat = location.lat
@@ -29,7 +32,7 @@ def map(request):
     country = location.country
 
     #creating Map Object with Folium
-    m = folium.Map(location=[-14.537928336797815, -40.95286772588722], zoom_start=3.5, width='100%', height='100%')
+    m = folium.Map(location=[-14.537928336797815, -40.95286772588722], zoom_start=3.5, width='100%', height='100%', tiles='OpenStreetMap')
 
     #folium.Marker([-12.988416362561837, -38.50353491104314]).add_to(m)
     #dá pra usar essa linha de baixo pra por os pontos turisticos com o marker + infos do nome do lugar
@@ -37,7 +40,7 @@ def map(request):
     folium.Marker([-22.90279798439243, -43.17928929265192], tooltip='Rio de Janeiro').add_to(m)
 
     # aqui vamo usar o form
-    folium.CircleMarker([lat, lng], tooltip=country).add_to(m)
+    folium.Circle([lat, lng], tooltip=country).add_to(m)
 
     #Representação do mapa para o html
     m = m._repr_html_()
